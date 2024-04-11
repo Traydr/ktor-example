31b33088d33baf58353db2c31809d9fd3d37017f
Traydr <Traydr@protonmail.com>
2024-04-11 10:53:43 +0300

    feat: init ktor

---
+++ src/main/kotlin/dev/traydr/vu/plugins/Security.kt
@@ -0,0 +1,15 @@
+package dev.traydr.vu.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+
+fun Application.configureSecurity() {
+    data class MySession(val count: Int = 0)
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+}
