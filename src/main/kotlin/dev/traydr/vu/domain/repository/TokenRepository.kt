ad887d0d123eb0afe5f2943e3a2d8300cf718612
Traydr <Traydr@protonmail.com>
2024-04-16 16:03:38 +0300

    wip: token repo

--- src/main/kotlin/dev/traydr/vu/domain/repository/TokenRepository.kt
+++ src/main/kotlin/dev/traydr/vu/domain/repository/TokenRepository.kt
@@ -4,11 +4,13 @@ import dev.traydr.vu.domain.Token
 import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.javatime.datetime
+import org.jetbrains.exposed.sql.transactions.transaction
 
 internal object Tokens : LongIdTable() {
     val user = reference("user", Users)
-    val value: Column<String> = varchar("value", 255)
+    val value: Column<String> = varchar("value", 255).uniqueIndex()
     val expiryDate = datetime("expiryDate")
 
     fun toDomain(row: ResultRow): Token {
@@ -18,5 +20,17 @@ internal object Tokens : LongIdTable() {
             value = row[value],
             expiryDate = row[expiryDate]
         )
+    }
+}
+
+class TokenRepository {
+    init {
+        transaction {
+            SchemaUtils.create(Tokens)
+        }
+    }
+
+    fun findTokenByEmail(email: String) {
+
     }
 }
