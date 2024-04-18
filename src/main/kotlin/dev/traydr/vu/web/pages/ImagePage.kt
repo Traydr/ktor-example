d81297649491ce84176e97ce8ab63f088e662186
Traydr <Traydr@protonmail.com>
2024-04-18 16:27:26 +0300

    feat: move image manipulation to new page

---
+++ src/main/kotlin/dev/traydr/vu/web/pages/ImagePage.kt
@@ -0,0 +1,47 @@
+package dev.traydr.vu.web.pages
+
+import dev.traydr.vu.utils.hxEncoding
+import dev.traydr.vu.utils.hxGet
+import dev.traydr.vu.utils.hxPost
+import dev.traydr.vu.web.components.footer
+import dev.traydr.vu.web.components.header
+import dev.traydr.vu.web.components.navbar
+import dev.traydr.vu.web.components.wrapper
+import kotlinx.html.*
+
+fun HTML.imagePage() {
+    header("Image Manipulator", "Upload and download images")
+    body {
+        navbar()
+        wrapper("Submitting Images") {
+            div {
+                classes = setOf("container", "mx-auto", "grid", "grid-cols-2", "gap-4")
+                div {
+                    form {
+                        hxPost("/api/v2/upload")
+                        hxEncoding("multipart/form-data")
+
+                        input {
+                            classes = setOf("file-input", "w-full", "max-w-xs")
+                            type = InputType.file
+                        }
+
+                        button {
+                            classes = setOf("btn btn-primary")
+                            type = ButtonType.submit
+                            +"Upload"
+                        }
+                    }
+                }
+                div {
+                    button {
+                        classes = setOf("btn btn-primary")
+                        hxGet("/api/v1/download/file.txt")
+                        +"Download"
+                    }
+                }
+            }
+        }
+        footer()
+    }
+}
--- src/main/kotlin/dev/traydr/vu/web/pages/IndexPage.kt
+++ src/main/kotlin/dev/traydr/vu/web/pages/IndexPage.kt
@@ -7,38 +7,11 @@ import dev.traydr.vu.web.components.*
 import kotlinx.html.*
 
 fun HTML.indexPage() {
-    header("File Manipulator", "Just a manipulator")
+    header("Index", "The home page")
     body {
-        classes = setOf("")
         navbar()
-        wrapper("Submitting Images") {
-            div {
-                classes = setOf("container", "mx-auto", "grid", "grid-cols-2", "gap-4")
-                div {
-                    form {
-                        hxPost("/api/v2/upload")
-                        hxEncoding("multipart/form-data")
+        wrapper("Home page") {
 
-                        input {
-                            classes = setOf("file-input", "w-full", "max-w-xs")
-                            type = InputType.file
-                        }
-
-                        button {
-                            classes = setOf("btn btn-primary")
-                            type = ButtonType.submit
-                            +"Upload"
-                        }
-                    }
-                }
-                div {
-                    button {
-                        classes = setOf("btn btn-primary")
-                        hxGet("/api/v1/download/file.txt")
-                        +"Download"
-                    }
-                }
-            }
         }
         footer()
     }
