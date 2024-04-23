244370a7ee65e3c74fb4c2378dbf92cb0e0e5cdb
Traydr <Traydr@protonmail.com>
2024-04-23 03:40:31 +0300

    feat: gs page finished mostly

--- src/main/kotlin/dev/traydr/vu/web/components/GlobalStoreComponents.kt
+++ src/main/kotlin/dev/traydr/vu/web/components/GlobalStoreComponents.kt
@@ -1,31 +1,38 @@
 package dev.traydr.vu.web.components
 
-import dev.traydr.vu.utils.hxPost
-import dev.traydr.vu.utils.hxPut
+import dev.traydr.vu.utils.*
 import kotlinx.html.*
 
 fun FlowContent.gsFormPost() = div {
     form {
+        classes = setOf("flex", "flex-col", "gap-2")
         hxPost("/api/v1/global")
 
-        input {
-            name = "key"
-            id = "input-key"
-            type = InputType.text
-            placeholder = "Key"
-            classes = setOf("input", "input-bordered", "w-full", "max-w-s")
+        label {
+            classes =
+                setOf("input", "input-bordered", "flex", "items-center", "input-accent", "gap-2")
+            +"Key"
+            input {
+                name = "key"
+                id = "input-key"
+                type = InputType.text
+                classes = setOf("grow")
+            }
         }
 
-        input {
-            name = "value"
-            id = "input-value"
-            type = InputType.text
-            placeholder = "Value"
-            classes = setOf("input", "input-bordered", "w-full", "max-w-s")
+        label {
+            classes =
+                setOf("input", "input-bordered", "flex", "items-center", "input-accent", "gap-2")
+            +"Value"
+            input {
+                name = "value"
+                id = "input-value"
+                type = InputType.text
+            }
         }
 
         button {
-            classes = setOf("btn btn-primary")
+            classes = setOf("btn", "btn-primary", "gap-2")
             +"Submit"
         }
     }
@@ -33,27 +40,69 @@ fun FlowContent.gsFormPost() = div {
 
 fun FlowContent.gsFormPut() = div {
     form {
+        classes = setOf("flex", "flex-col", "gap-2")
         hxPut("/api/v1/global")
 
-        input {
-            name = "key"
-            id = "existing-key"
-            type = InputType.text
-            placeholder = "Key"
-            classes = setOf("input", "input-bordered", "w-full", "max-w-s")
+        label {
+            classes =
+                setOf("input", "input-bordered", "flex", "items-center", "input-accent", "gap-2")
+            +"Key"
+            input {
+                name = "key"
+                id = "existing-key"
+                type = InputType.text
+            }
         }
 
-        input {
-            name = "value"
-            id = "new-value"
-            type = InputType.text
-            placeholder = "Value"
-            classes = setOf("input", "input-bordered", "w-full", "max-w-s")
+        label {
+            classes =
+                setOf("input", "input-bordered", "flex", "items-center", "input-accent", "gap-2")
+            +"Value"
+            input {
+                name = "value"
+                id = "new-value"
+                type = InputType.text
+            }
         }
 
         button {
             classes = setOf("btn btn-primary")
             +"Update"
+        }
+    }
+}
+
+fun FlowContent.gsFormGet() = div {
+    form {
+        classes = setOf("flex", "flex-col", "gap-2")
+        hxGet("/api/v1/global")
+        hxTarget("#gs-output")
+        hxSwap("innerHTML")
+
+        label {
+            classes =
+                setOf("input", "input-bordered", "flex", "items-center", "input-accent", "gap-2")
+            +"Key"
+            input {
+                name = "key"
+                id = "output-key"
+                type = InputType.text
+            }
+        }
+
+        button {
+            classes = setOf("btn", "btn-primary")
+            +"Display"
+        }
+
+        div {
+            id = "gs-output"
+            role = "alert"
+            classes = setOf(
+                "alert",
+                "alert-info"
+            )
+            +"Output will be displayed here"
         }
     }
 }
--- src/main/kotlin/dev/traydr/vu/web/pages/DatabasePage.kt
+++ src/main/kotlin/dev/traydr/vu/web/pages/DatabasePage.kt
@@ -11,61 +11,23 @@ fun HTML.databasePage() {
         wrapper("Global key-value store") {
             div {
                 classes = setOf("container", "mx-auto", "grid", "grid-cols-2", "gap-4")
-                div {
-                    classes = setOf("flex", "justify-center", "grid", "grid-cols-1")
 
-                    h2 {
-                        classes = setOf("text-xl", "p-4")
-                        +"You will be submitting a key-value pair to a global store"
-                    }
-
-                    gsFormPost()
-                }
-                div {
-                    classes = setOf("flex", "justify-center", "grid", "grid-cols-1")
-
-                    h2 {
-                        classes = setOf("text-xl", "p-4")
-                        +"You will be retrieving a value from the global key-value store"
-                    }
-
-                    form {
-                        hxGet("/api/v1/global")
-                        hxTarget("#state-output")
-                        hxSwap("innerHTML")
-
-                        input {
-                            name = "key"
-                            id = "output-key"
-                            type = InputType.text
-                            placeholder = "Key"
-                            classes = setOf("input", "input-bordered", "w-full", "max-w-s")
-                        }
-
-                        button {
-                            classes = setOf("btn", "btn-primary")
-                            +"Display"
-                        }
-                    }
-
-                    div {
-                        id = "state-output"
-                        classes = setOf("text-center", "outline", "outline-offset-0", "outline-orange-500")
-                        +"Output will be displayed here"
-                    }
-                }
-                div {
-                    classes = setOf("flex", "justify-center", "grid", "grid-cols-1")
-
-                    h2 {
-                        classes = setOf("text-xl", "p-4")
-                        +"You will be updating a key-value pair to a global store"
-                    }
-
-                    gsFormPut()
-                }
+                gsFormTempl({ gsFormPost() }, "You will be submitting a key-value pair to a global store")
+                gsFormTempl({ gsFormGet() }, "You will be retrieving a value from the global key-value store")
+                gsFormTempl({ gsFormPut() }, "You will be updating a key-value pair to a global store")
             }
         }
         footer()
     }
+}
+
+fun FlowContent.gsFormTempl(unit: () -> Unit, title: String) = div {
+    classes = setOf("flex", "justify-center", "grid", "grid-cols-1")
+
+    h2 {
+        classes = setOf("text-xl", "p-4", "text-center")
+        +title
+    }
+
+    unit()
 }
