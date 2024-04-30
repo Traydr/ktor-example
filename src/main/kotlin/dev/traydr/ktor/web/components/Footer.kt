8ead7e75cfff35398a49e6c6de1b5803a6890d8c
Traydr <Traydr@protonmail.com>
2024-04-30 18:39:16 +0300

    feat: add GitHub logo to footer and change to made by Traydr

--- src/main/kotlin/dev/traydr/ktor/web/components/Footer.kt
+++ src/main/kotlin/dev/traydr/ktor/web/components/Footer.kt
@@ -7,17 +7,34 @@ fun FlowContent.footer() = footer {
     classes = setOf(
         "footer",
         "footer-center",
+        "p-10",
         "bg-base-100/75",
-        "mt-auto",
         "text-xl",
         "outline",
         "outline-offset-0",
         "outline-blue-800"
     )
     aside {
+        classes = setOf()
         p {
             classes = setOf("font-bold")
-            +"Made by Titas Lukaitis"
+            +"Made by Traydr"
+        }
+    }
+
+    nav {
+        div {
+            classes = setOf("grid", "grid-flow-col", "gap-4")
+            a {
+                href = "https://github.com/Traydr/ktor-example"
+                img {
+                    classes = setOf("")
+                    src = "/logos/github-mark-white.svg"
+                    alt = "Github"
+                    width = "32"
+                    height = "32"
+                }
+            }
         }
     }
 }
