6ee99fa6a09cc6e858b85eb240f6a7523427a02d
Traydr <Traydr@protonmail.com>
2024-04-30 18:44:24 +0300

    feat: add blurb to index

--- src/main/kotlin/dev/traydr/ktor/web/pages/IndexPage.kt
+++ src/main/kotlin/dev/traydr/ktor/web/pages/IndexPage.kt
@@ -8,7 +8,16 @@ fun HTML.indexPage() {
     body {
         navbar()
         wrapper("Home") {
-
+            p {
+                classes = setOf("text-center")
+                +"I made this website to show the capabilities of Ktor and how other kotlin libraries interact with it"
+                br { }
+                +"This is my first project using kotlin and ktor, so expect that some things are not optimal, and that I may not be doing them correctly"
+            }
+            p {
+                classes = setOf("text-center")
+                +"You may view the source code through the link below"
+            }
         }
         footer()
     }
